4bd70696199cafbd0f7e61b9736eefef7d9e5508
Caroline Illemann Hansen <caro.illemann@gmail.com>
2023-01-25 12:41:51 +0100

    fix(index): scss import fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import * as sharedJs from "./auto-generated/dist/js/_shared.js";
-import * as sharedScss from "./auto-generated/dist/scss/_shared.scss";
+import * as sharedScss from "./auto-generated/dist/scss/_shared.scss?inline";
 
 export default {
   shared: {
