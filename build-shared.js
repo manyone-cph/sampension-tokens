d77ffbf13ca23512f4c3cb96eb54e4dfb9299ed6
Lau Rasmussen <laurasmussen@mac.com>
2023-01-25 23:09:17 +0100

    Make better map

--- build-shared.js
+++ build-shared.js
@@ -1,32 +1,97 @@
 // build.js
 const StyleDictionary = require("style-dictionary");
 
+// Convert to rem
 const cleanRemSize = (val) => {
+  // if the value is a string, remove the px and convert to rem
+  if (typeof val == "string" && val.includes("px")) {
+    val = Number(val.replace("px", ""));
+  }
+
   if (typeof val == "number") {
     return val / 16 + "rem";
   } else {
     return val;
   }
 };
-// Convert to rem
+
+// size/toREM -> Collection that needs converting to rem 
 StyleDictionary.registerTransform({
   name: "size/toREM",
   type: "value",
   matcher: function (prop) {
-    console.log(prop);
+    console.log(prop.type);
     return (
       prop.type === "fontSizes" ||
+      prop.type === "lineHeights" || prop.type === "lineHeight" ||
       prop.type === "spacing" ||
       prop.type === "sizing" ||
-      (prop.path.includes("typography") && prop.path.includes("fontSize"))
+      prop.type === "dimension" ||
+      prop.type === "borderRadius" ||
+      (prop.path.includes("typography") && prop.path.includes("fontSize")) ||
+      (prop.path.includes("typography") && prop.path.includes("lineHeights"))
     );
   },
   transformer: function (prop) {
     return cleanRemSize(prop.value);
   },
 });
-// This code takes the tokens from the tokens/shared.json folder
-// SCSS file with a _core.scss file and a JS file with a core.js file.
+
+// border/toShorthand -> Convert border object to border shorthand
+StyleDictionary.registerTransform({
+  name: "border/toShorthand",
+  type: "value",
+  matcher: function (prop) {
+    return prop.type === "border";
+  },
+  transformer: function (prop) {
+    return `${cleanRemSize(prop.value.width)} ${prop.value.style} ${prop.value.color}`;
+  },
+});
+
+// fontWeights/toNumber -> Convert fontWeights to numbers
+StyleDictionary.registerTransform({
+  name: "fontWeights/toNumber",
+  type: "value",
+  matcher: function (prop) {
+    return prop.type === "fontWeights";
+  },
+  transformer: function (prop) {
+    // We could make this more robust by checking for all possible font weight values from figma
+    switch (prop.value.toLowerCase()) {
+      case "thin":
+        return 100;
+      case "extralight":
+        return 200;
+      case "light":
+        return 300;
+      case "regular":
+        return 400;
+      case "medium":
+        return 500;
+      case "bold":
+        return 700;
+      case "black":
+        return 700;
+      default:
+        return 400;
+    }
+  },
+});
+
+// Convert fontFamily to css variable
+StyleDictionary.registerTransform({
+  name: "fontFamily/toCSSVariable",
+  type: "value",
+  matcher: function (prop) {
+    return prop.type === "fontFamilies";
+  },
+  transformer: function (prop) {
+    return `var(--ff-${prop.value.toLowerCase()})`;
+  },
+});
+
+
 
 const filePaths = {
   shared: "tokens/shared.json"
@@ -38,7 +103,13 @@ const myStyleDictionary = StyleDictionary.extend({
     scss: {
       transformGroup: "scss",
       buildPath: "./dist/scss/",
-      transforms: ["name/cti/kebab", "size/toREM"],
+      transforms: [
+        "name/cti/kebab",
+        "size/toREM",
+        "border/toShorthand",
+        "fontWeights/toNumber",
+        "fontFamily/toCSSVariable",
+      ],
       files: [
         {
           destination: "_shared.scss",
