7464476bb169bee7460240d87a7d563498724002
Caroline Illemann Hansen <caro.illemann@gmail.com>
2023-02-07 10:59:04 +0100

    no message

---
+++ build/release.packageVersion.mjs
@@ -0,0 +1,10 @@
+import path from "path";
+import fs from "fs-extra";
+
+console.log("🟣 Update package version");
+// create package.json file for the package
+// const packageJson = fs.readFileSync(path.resolve(__dirname, "../package.json"), "utf8");
+// const packageJsonParsed = JSON.parse(packageJson);
+// const packageJsonTemplate = fs.readFileSync(path.resolve(__dirname, "./templates/package.json"), "utf8");
+// const newPackageJson = packageJsonTemplate.replace(/__VERSION__/g, packageJsonParsed.version);
+// const newPackageJsonParsed = JSON.parse(newPackageJson);
